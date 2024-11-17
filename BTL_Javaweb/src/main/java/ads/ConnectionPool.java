09a47be28034f3d1358c936147594d9a317f8abc
Hungdx2003 <chienhon2003@gmail.com>
2024-11-17 17:47:57 +0700

    ma nguon btl

--- BTL_Javaweb/src/main/java/ads/ConnectionPool.java
+++ BTL_Javaweb/src/main/java/ads/ConnectionPool.java
@@ -6,4 +6,5 @@ import java.sql.SQLException;
 public interface ConnectionPool {
 	Connection getConnection(String objectName) throws SQLException;
 	void releaseConnection(Connection con, String objectName) throws SQLException;
+	
 }
